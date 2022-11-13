3285f1179711f17afc6cb92e9435c834867d8801
Tanya Yenakieva <yastigma@gmail.com>
2022-11-13 16:58:31 +0200

    Update script.js

--- script.js
+++ script.js
@@ -29,23 +29,61 @@
 //       }
 
 
-const apartment = {
-    descr: "Spacious apartment in the city center",
-    rating: 4,
-    price: 2153,
-  };
-   const keys = [];
-  const values = [];
+// const apartment = {
+//     descr: "Spacious apartment in the city center",
+//     rating: 4,
+//     price: 2153,
+//   };
+//    const keys = [];
+//   const values = [];
 
-  for (const key in apartment){
-    keys.push(key);
-    values.push(apartment[key]);
-    //   console.log(key) ;
-  }
-  console.log(keys) ;
-  console.log(values) ;
+//   for (const key in apartment){
+//     keys.push(key);
+//     values.push(apartment[key]);
+//     //   console.log(key) ;
+//   }
+//   console.log(keys) ;
+//   console.log(values) ;
   
   // Change code below this line
+
+  // function countTotalSalary(salaries) {
+  //   let totalSalary = 0;
+  //   // Change code below this line
+  // let salary = Object.values(salaries);
+  // for (const value of salary) {
+  // totalSalary += value;}
   
   
+  //   // Change code above this line
+  //   return totalSalary;
+
+  // }
+    // console.log(value);
   
+    const products = [
+      { name: "Radar", price: 1300, quantity: 4 },
+      { name: "Scanner", price: 2700, quantity: 3 },
+      { name: "Droid", price: 400, quantity: 7 },
+      { name: "Grip", price: 1200, quantity: 9 },
+    ];
+    
+    function calculateTotalPrice(productName) {
+      // Change code below this line
+      let totalPrice = a * b; 
+      let a = 0;
+      let b = 0;
+    for (const product of products) {
+      if (productName === product.name) {
+
+        a = (product.price);
+        b = (product.quantity);
+          return totalPrice;
+      }
+     
+    }
+   
+    
+      // Change code above this line
+    }
+    // console.log(totalPrice);
