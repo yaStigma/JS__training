b5c8068c816b85b3f8859dccd3b6977ae0ece26e
Tanya Yenakieva <yastigma@gmail.com>
2023-10-29 19:47:44 +0200

    task 47-48 final

--- script.js
+++ script.js
@@ -606,6 +606,8 @@
 //     rating: 8.67,
 //   },
 // ];
+
+
 // const MIN_BOOK_RATING = 8;
 // // Change code below this line
 
@@ -617,4 +619,90 @@
 // console.log(names);
 
 // task 47
+
+// const  users =[
+//   {
+//     name: "Moore Hensley",
+//     email: "moorehensley@indexia.com",
+//     eyeColor: "blue",
+//     friends: ["Sharron Pace"],
+//     isActive: false,
+//     balance: 2811,
+//     gender: "male"
+//   },
+//   {
+//     name: "Sharlene Bush",
+//     email: "sharlenebush@tubesys.com",
+//     eyeColor: "blue",
+//     friends: ["Briana Decker", "Sharron Pace"],
+//     isActive: true,
+//     balance: 3821,
+//     gender: "female"
+//   },
+//   {
+//     name: "Ross Vazquez",
+//     email: "rossvazquez@xinware.com",
+//     eyeColor: "green",
+//     friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+//     isActive: false,
+//     balance: 3793,
+//     gender: "male"
+//   },
+//   {
+//     name: "Elma Head",
+//     email: "elmahead@omatom.com",
+//     eyeColor: "green",
+//     friends: ["Goldie Gentry", "Aisha Tran"],
+//     isActive: true,
+//     balance: 2278,
+//     gender: "female"
+//   },
+//   {
+//     name: "Carey Barr",
+//     email: "careybarr@nurali.com",
+//     eyeColor: "blue",
+//     friends: ["Jordan Sampson", "Eddie Strong", "Adrian Cross"],
+//     isActive: true,
+//     balance: 3951,
+//     gender: "male"
+//   },
+//   {
+//     name: "Blackburn Dotson",
+//     email: "blackburndotson@furnigeer.com",
+//     eyeColor: "brown",
+//     friends: ["Jacklyn Lucas", "Linda Chapman", "Adrian Cross", "Solomon Fokes"],
+//     isActive: false,
+//     balance: 1498,
+//     gender: "male"
+//   },
+//   {
+//     name: "Sheree Anthony",
+//     email: "shereeanthony@kog.com",
+//     eyeColor: "brown",
+//     friends: ["Goldie Gentry", "Briana Decker"],
+//     isActive: true,
+//     balance: 2764,
+//     gender: "female"
+//   }
+// ];
+
+// const friends = users
+//     .map(users => users.friends)
+//     .flat()
+//     .filter((friends, index, array) => array.indexOf(friends) === index)
+
+//     .sort((a, b) => a.localeCompare(b));
+//     ;
+
+//     console.log(friends);
+
+// task 48
+
+// const getTotalBalanceByGender = (users, gender) => {
+//   const totalBalance = users
+//     .filter(user => user.gender === gender) // Фильтруем пользователей по полу
+//     .reduce((accumulator, user) => accumulator + user.balance, 0);
+
+//   return totalBalance;
+// };
 
